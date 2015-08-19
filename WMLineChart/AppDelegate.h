562102434772b269ad7eb253f4746e6affbededc
wangwendong <wangwendong@wangwendongs-MacBook-Pro.local>
2015-08-19 21:07:39 +0800

    Initial Commit

---
+++ WMLineChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMLineChart
+//
+//  Created by wangwendong on 15/8/19.
+//  Copyright (c) 2015年 wangwendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WMLineChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMLineChart
+//
+//  Created by wangwendong on 15/8/19.
+//  Copyright (c) 2015年 wangwendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
